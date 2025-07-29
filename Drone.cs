87de3ae85099a4c7a56b1ad1e71db41179afac95
30105350 <30105350@RK-F30-610325>
2025-07-29 11:53:14 +0800

    Add project files.

---
+++ Drone.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dr
+{
+    public class Drone
+    {
+        private string ClientName { get; set; }
+        private string DroneModel { get; set; }
+        private string Problem { get; set; }
+        private double Cost { get; set; }
+        private string Tag { get; set; }
+        public Drone()
+        {
+
+        }
+        public Drone(string model, string problem, string client, double cost, string tag)
+        {
+            this.ClientName = model;
+            this.Problem = problem;
+            this.Cost = cost;
+            this.Tag = tag;
+            this.DroneModel = model;
+        }
+
+    }
+}
---
+++ DroneController.cs
@@ -0,0 +1,64 @@
+﻿using Dr;
+using Comp;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Serialization;
+namespace C_2AT1
+{
+    public class Controller
+    {
+        Queue<Drone> Express;
+        Queue<Drone> Regular;
+        List<Completed> Completed;
+        public Controller() 
+        {
+            Regular = new Queue<Drone>();
+            Express = new Queue<Drone>();
+            Completed = new List<Completed>();
+        }
+
+        public void DroneAddReg(string model, string problem, string client, double cost, string tag)
+        {
+            Regular.Enqueue(new Drone(model, problem, client, cost, RegTagGen()));
+        }
+        public void DroneAddExp(string model, string problem, string client, double cost, string tag)
+        {
+            Express.Enqueue(new Drone(model, problem, client, ExpCostCalc(cost,0.15), ExpTagGen()));
+        }
+        public void DroneRemoveReg()
+        {
+            Regular.Dequeue();
+        }
+        public void DroneRemoveExp()
+        {
+            Express.Dequeue();
+        }
+        public double ExpCostCalc(double cost,double surcharge)
+        {
+            return cost + (cost * surcharge);
+        }
+        public string ExpTagGen()
+        {
+            return $"#{Express.Count + 1}"; 
+        }
+        public string RegTagGen()
+        {
+            return $"#{Regular.Count + 1}";
+        }
+        public void AddCompleteList(int choice)
+        {
+            if (choice == 1)
+            {
+                Completed.Add(Express.ToList());
+            }
+            else if (choice == 2) 
+            {
+
+            }
+        }
+        public void convert()
+        {
+            Express.Peek();
+            Express.ToList();
+        }
+    }
+}
